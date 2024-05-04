d81a3262cfab04a24097f41fc292d6fdd1846fdb
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-04 19:42:24 -0300

    Update register_process.php

--- register_process.php
+++ register_process.php
@@ -1,9 +1,20 @@
 <?php
     include 'db.php';
     $username = $_POST['username'];
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
-    $stmt->bind_param("ss", $username, $password);
+    $password = $_POST['password'];
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ?");
+    $stmt->bind_param("s", $username);
     $stmt->execute();
-    header("Location: login.php");
+    $result = $stmt->get_result();
+    if($result->num_rows > 0){
+        header("Location: register.php?error=username_taken");
+    }else{
+        $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+        $stmt = $conn->prepare("INSERT INTO users (username, password) VALUES (?, ?)");
+        $stmt->bind_param("ss", $username, $hashedPassword);
+        $stmt->execute();
+        header("Location: login.php");
+    }
+    $stmt->close();
+    $conn->close();
 ?>
