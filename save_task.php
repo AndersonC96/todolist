671f05b93a60c8685eb002ecab0869210e7047ca
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-04 22:18:06 -0300

    Update save_task.php

--- save_task.php
+++ save_task.php
@@ -3,9 +3,10 @@
     session_start();
     $title = $_POST['title'];
     $description = $_POST['description'];
+    $due_date = $_POST['due_date'];
     $username = $_SESSION['username'];
-    $stmt = $conn->prepare("INSERT INTO tasks (title, description, username) VALUES (?, ?, ?)");
-    $stmt->bind_param("sss", $title, $description, $username);
+    $stmt = $conn->prepare("INSERT INTO tasks (title, description, due_date, username) VALUES (?, ?, ?, ?)");
+    $stmt->bind_param("ssss", $title, $description, $due_date, $username);
     $stmt->execute();
     header("Location: index.php");
 ?>
