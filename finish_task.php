2e95bdc27f88d255b02b8fe56da51c6a418bca51
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-04 23:20:20 -0300

    Create finish_task.php

---
+++ finish_task.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    include 'db.php';
+    if(!isset($_SESSION['username'])){
+        header("Location: login.php");
+        exit();
+    }
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $taskId = $_POST['task_id'];
+        $stmt = $conn->prepare("UPDATE tasks SET completed = '1' WHERE id = ?");
+        $stmt->bind_param("i", $taskId);
+        if($stmt->execute()){
+            header("Location: index.php");
+        }else{
+            echo "Erro ao concluir a tarefa.";
+        }
+        $stmt->close();
+    }
+?>
