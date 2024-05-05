13ce824316e85887dcd3620c67e123d694753b60
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-04 23:20:11 -0300

    Create finish_subtask.php

---
+++ finish_subtask.php
@@ -0,0 +1,19 @@
+<?php
+    session_start();
+    include 'db.php';
+    if(!isset($_SESSION['username'])){
+        header("Location: login.php");
+        exit();
+    }
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        $subtaskId = $_POST['subtask_id'];
+        $stmt = $conn->prepare("UPDATE subtasks SET completed = 'concluída' WHERE id = ?");
+        $stmt->bind_param("i", $subtaskId);
+        if($stmt->execute()){
+            header("Location: index.php");
+        }else{
+            echo "Erro ao concluir a subtarefa.";
+        }
+        $stmt->close();
+    }
+?>
