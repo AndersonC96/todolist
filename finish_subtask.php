2e6f1afce2ea5aca6c1ad6d7c1693dae5e4a7d6d
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-04 23:31:38 -0300

    Update finish_subtask.php

--- finish_subtask.php
+++ finish_subtask.php
@@ -7,7 +7,7 @@
     }
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $subtaskId = $_POST['subtask_id'];
-        $stmt = $conn->prepare("UPDATE subtasks SET completed = 'Concluída' WHERE id = ?");
+        $stmt = $conn->prepare("UPDATE subtasks SET completed = 1 WHERE id = ?");
         $stmt->bind_param("i", $subtaskId);
         if($stmt->execute()){
             header("Location: index.php");
