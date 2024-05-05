06a37c6c09187469a80846dbbf7938771280f346
Anderson Cavalcante Barbosa <andersoncavalcantr96@hotmail.com>
2024-05-04 23:30:57 -0300

    Update finish_task.php

--- finish_task.php
+++ finish_task.php
@@ -7,7 +7,7 @@
     }
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         $taskId = $_POST['task_id'];
-        $stmt = $conn->prepare("UPDATE tasks SET completed = 'Concluída' WHERE id = ?");
+        $stmt = $conn->prepare("UPDATE tasks SET completed = 1 WHERE id = ?");
         $stmt->bind_param("i", $taskId);
         if($stmt->execute()){
             header("Location: index.php");
